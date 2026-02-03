a18385e8e6c772b4231289408dbff313091de5e0
Roberto Silvestri <roberto@gotham.studio>
2026-02-03 11:41:07 +0100

    fix: utils deps

--- src/runtime/plugins/02.utils.js
+++ src/runtime/plugins/02.utils.js
@@ -1,12 +1,11 @@
 import { defineNuxtPlugin } from '#app'
-import { useLanguage, useRoute, useRuntimeConfig, useStoryblokEditor } from '#imports'
+import { useLanguage, useRoute, useRuntimeConfig, useStoryblokEditor } from '#imports';
 
 export default defineNuxtPlugin({
   dependsOn: ['labels', 'images'],
   async setup() {
     const config = useRuntimeConfig()
-    const { storyblok } = config.gothamstoryblok
-    const { version } = storyblok
+    const version = config.public?.storyblok?.version || config.gothamstoryblok?.storyblok?.version || 'public';
     return {
       provide: {
         __url: (u) => {
