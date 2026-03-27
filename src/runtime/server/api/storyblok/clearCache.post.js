00b918dee87781698ed28b9fca46107681e56014
Umberto <umberto@gotham.studio>
2026-03-27 10:55:22 +0100

    chore: pre-release changes

--- src/runtime/server/api/storyblok/clearCache.post.js
+++ src/runtime/server/api/storyblok/clearCache.post.js
@@ -1,15 +1,56 @@
 import { clearLinks } from '../../utils/storyblok'
-import { defineEventHandler, useStorage } from '#imports'
+import { defineEventHandler, useStorage, readBody, createError } from '#imports'
 
-export default defineEventHandler(async () => {
-  // Clear cache Nitro
-  const cacheStorage = useStorage('cache:storyblok:_')
-  const cachedKeys = await cacheStorage.getKeys()
-  let promiseArr = []
-  for (var i in cachedKeys) promiseArr.push(cacheStorage.removeItem(cachedKeys[i]))
-  await Promise.all(promiseArr)
+export default defineEventHandler(async (event) => {
+  try {
+    const body = await readBody(event)
+    const { full_slug } = body
+    const store = useStorage('cache:storyblok:_')
+    const cachedKeys = await store.getKeys()
+    await clearLinks()
 
-  await clearLinks()
+    if (full_slug) {
+      const foundKeys = []
+      for (const key of cachedKeys) {
+        const stored = await store.getItem(key)
+        const found = searchInCache(stored, full_slug)
 
-  return true
+        if (found) {
+          foundKeys.push(key)
+          await store.removeItem(key)
+        }
+      }
+
+      return { message: 'cache cleared by full_slug', found: foundKeys.length > 0, keys: foundKeys }
+    }
+    else {
+      let promiseArr = []
+      for (var i in cachedKeys) promiseArr.push(store.removeItem(cachedKeys[i]))
+      await Promise.all(promiseArr)
+
+      return { message: 'full cache cleared' }
+    }
+  }
+  catch (error) {
+    console.log(error)
+    throw createError({ statusCode: 500, statusMessage: 'Error clearing cache' })
+  }
 })
+
+const searchInCache = (stored, full_slug) => {
+  if (!stored || !full_slug) return false
+
+  const body = stored?.value?.body
+  if (!body) return false
+
+  if (typeof body === 'object') {
+    if (body.full_slug === full_slug) return true
+  }
+
+  if (Array.isArray(body)) {
+    for (const story of body) {
+      if (!story) continue
+      if (story.full_slug === full_slug) return true
+    }
+  }
+}
